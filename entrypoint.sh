05e211931d2d0f23e6bb93a712d9ae576717dabb
Pini Vaknin <pini@bluebricks.co>
2025-03-16 21:19:54 +0200

    improve svg render

--- entrypoint.sh
+++ entrypoint.sh
@@ -229,9 +229,9 @@ case "$INPUT_COMMAND" in
     # Embed SVG visualization from a temporary file
     temp_svg_file="${GITHUB_WORKSPACE}/43e12552-4a61-41cc-a9b3-f5ce8c18e8b5.svg"
     
-    #TODO: Remove once done testing
+    #TODO: Remove once done
     if [ -f "$temp_svg_file" ]; then
-      svg_content=$(cat "$temp_svg_file")
+      svg_content=$(cat "$temp_svg_file" | sed 's/$/\\n/g' | tr -d '\n')
       
       echo "### 📊 Deployment Visualization" >> "$GITHUB_STEP_SUMMARY"
       echo "<details>" >> "$GITHUB_STEP_SUMMARY"
